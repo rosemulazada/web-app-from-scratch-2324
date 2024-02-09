ce8f5017692364a481f98de2d3d2a1bcace2246c
rosemulazada <rosemulazada16@gmail.com>
2024-02-09 12:37:59 +0100

    created array that stores all data we call from the API. wrote a for loop that logs something to the console once for the length of the array. this is a setup for dynamically loading in images in the next update.

--- docs/scripts/script.js
+++ docs/scripts/script.js
@@ -10,21 +10,16 @@ let imageElement = document.createElement("img");
 let dynamicImagePath;
 let flipCard = document.getElementsByClassName("flip-card__back-side");
 let hasInteracted = false;
+// maak een array aan om later alle opgehaalde data uit de API in te storen
+// dit doe ik zodat ik straks kan loopen adhv de length van deze data.
+let allEntryData = [];
 
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("page is fully loaded");
-  getHyruleCompendiumAPI(155);
-  getHyruleCompendiumAPI(156);
-  getHyruleCompendiumAPI(157);
-  getHyruleCompendiumAPI(158);
-});
-
+// deze functie neemt als parameter 'entryNumber', dit is het getal van de entry van het karakter dat je wilt oproepen uit de API.
 async function getHyruleCompendiumAPI(entryNumber) {
   const response = await fetch(
     `https://botw-compendium.herokuapp.com/api/v3/compendium/entry/${entryNumber}?game=botw`
   );
   dataStorage = await response.json();
-
   // for ease of access, i store the data json inside another variable called 'data.'
   // this is to prevent having to call keys out of this variable with 'data.data' later on,
   // because of how the object is structured.
@@ -36,7 +31,27 @@ async function getHyruleCompendiumAPI(entryNumber) {
 
   imageElement.src = `${dynamicImagePath}`;
   imageElement.alt = `${data.name}`;
+
+  allEntryData.push({
+    entryNumber: entryNumber,
+    data: data,
+    dynamicImagePath: dynamicImagePath,
+  });
 }
+
+document.addEventListener("DOMContentLoaded", async () => {
+  // ik roep de data vooraf op, zodat de pagina de benodigde data om in te laden al heeft.
+  // voer de benodigde entryNumbers in.
+  // learned something about await; dont forget to document.
+  await getHyruleCompendiumAPI(155);
+  await getHyruleCompendiumAPI(156);
+  await getHyruleCompendiumAPI(157);
+  await getHyruleCompendiumAPI(158);
+  console.log("ALL ENTRY DATA", allEntryData);
+  for (let i = 0; i < allEntryData.length; i++) {
+    console.log("allEntryData logged with for loop");
+  }
+});
 
 // this variable contains all of my dynamic page data for this project, save from of course the API.
 // i store the html of the desired pages in an Object, since this is meant to be a one pager.
