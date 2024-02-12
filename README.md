0fc2ddfe65218a89c826933cb2aefa421f71bc2c
rosemulazada <rosemulazada16@gmail.com>
2024-02-12 09:52:59 +0100

    push forcefully

--- README.md
+++ README.md
@@ -1,60 +1,121 @@
-# Web App From Scratch @cmda-minor-web 2023 - 2024
-
-In dit vak gaan we een web applicatie bouwen en leren hoe deze werken door zo min mogelijk libraries, frameworks of
-andere bronnen te importen (vanilla) en zoveel mogelijk het browser platform te gebruiken door middel van HTML, CSS &
-JavaScript.    
-Het eindresultaat is een modulaire, single page web app (SPA), beoordeeld op jouw niveau en eigen leerdoelen.    
-De bedoeling is om een web app te bouwen die data ophaalt van een externe API, deze manipuleert om te tonen in de UI van
-de App, hoe abstract dan ook.
-Je gaat bekende patterns leren toe passen bij het bouwen en testen van de applicatie voor een eigen "WebSite" en een
-gezamenlijke "Team App".
-Nog nooit heb je zo snel kunnen prototypen als nadat je hebt leren werken met de browser en haar talen.
-Daarnaast zal je tijdloze kennis opdoen over het de aard en het gebruik van data, datastructuren, algoritmes, libraries,
-frameworks en de werking van het web.
-
-## Assignment
-1. Bouw je eigen SPA/WebSite met externe data van een API, minimaal 1 micro interactie.
-2. Een TeamApp met een verzameling van alle losse websites van je teamleden
-
----
-
-## Program
-
-| Planning | Maandag               | Dinsdag                | Woensdag                           | Donderdag                   | Vrijdag                                   |
-|----------|-----------------------|------------------------|------------------------------------|-----------------------------|-------------------------------------------|
-|          | Kick-Off, Dev WebSite | Dev TeamApp, Workshops | Dev *, Weekly nerd                 | Dev *, Workshops            | Code review, Voortgangsgesprekken, Dev *  |
-|          | Dev *, Workshops      | College + Workshops    | Dev *, Weekly nerd, Deadline 23:59 | Mondeling, Weekly Nerd Blog | Reparatiegesprekken, Afsluiting, 🍻 Fest? |
-
-## Rubric
-
-Je inzet wordt beoordeeld met behulp van de rubric (zie hieronder). Je moet het criterium (middenkolom) behalen om het
-vak te voltooien.
-Tijdens de toets wordt je mondeling overhoord en krijg je feedback over dingen die we denken dat tekort schieten en dingen die
-we denken dat een verbetering zijn op het criterium.
-
-TBA
-
-[//]: # ()
-[//]: # (| Deficiency | Criterion                                                                                                                                                                              | Improvement |)
-
-[//]: # (|:-----------|:---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|:------------|)
-
-[//]: # (|            | *User Interface* - you design, build and test the user interface by applying interface design principles                                                                               |             |)
-
-[//]: # (|            | *Code structure* - you write modular, consistent and efficient HTML, CSS and JavaScript code by applying structure and best practices. You manage state for the application and the UI |             |)
-
-[//]: # (|            | *Data management* - you understand how you can work with an external API using asynchronous code. You can retrieve data, manipulate and dynamically convert it to structured html      |             |)
-
-[//]: # (|            | *Project* - your app is working and published on GitHub Pages. Your project is thoroughly documented in the `README.md` file in your repository.                                       |             |)
-
 <!-- Add a link to your live demo in Github Pages 🌐-->
-#De opdrachten
 
-##Opdracht 1: De persoonlijke website
-De persoonlijke website staat online via Github Pages:
+#The assignment
+
+##Part 1: The Personal Website
+My personal site is deployed on Github Pages and can be accessed here:
 https://rosemulazada.github.io/web-app-from-scratch-2324/
 
 <!-- ☝️ replace this description with a description of your own work -->
+
+## Table of Contents
+
+- [Introduction](#introduction)
+- [Installation](#installation)
+- [Usage and Features](#usage-and-features)
+- [Checklist / Wishlist](#checklist--wishlist)
+- [Process](#process)
+- [External Data Sources](#external-data-sources)
+
+## Introduction
+
+We're kicking off the subject of Web Apps from Scratch on February 5th, and we're doing it with a personal website that should hold a JSON file with information about yourself and should receive data from an API. This data must be then collected in the second part of this assignment, the team application, in which the collective data of four sites is gathered to make a new website, for the teachers who can then take the students team apps and create something using that data. It's definitely curious and I'm excited to see how it will end up going! For now, let's start at the basics.
+
+## Installation
+
+Here are the steps to install my project.
+
+1. Under the `Code` tab at the top (the big green button).
+2. Download this repository as a `.zip` file and unpack it.
+3. Locate the folder `web-app-from-scratch-2324.`
+   - If you have `VS Code`: From here, all you need to do is open the folder in your editor and use the `Live Server` extension to start a local host. This allows you to also view the code and work more hands-on, should you desire.
+   - If not, you can find the document on your device, navigate to the `index.html` file (this should be visible just by opening up `web-app-from-scratch-2324`), right-click it and open in a browser of your choosing: I can recommend Google Chrome, as this is the browser I've used personally.
+
+## Usage and features
+
+This is how you can use my project.
+
+## Checklist / Wishlist
+
+All of these points and, if necessary, possible revisions will be listed and addressed in detail in the next section: [Process](#process).
+
+1 .✅ Draggable master sword
+
+- Possible revision here for accessibility reasons: dragging is difficult on a keyboard.. View reference for this piece of code in the [External Data Sources](#external-data-sources) section.
+
+2. ✅ Music syncing up with the dragging of the sword and the transition to the next 'page' (more on this in point 3).
+   - Initially, I had one full audio element, but this became hard to time since I'd need to time it perfectly with a setInterval(). Later on, I ended up splitting this file into two parts: when the first audio stops, the page transitions, and when it does, the next half plays.
+3. ✅ Dynamic page loading (considering this is meant to be a one-pager) ✅
+   - Using an object, I'm dynamically updating the content of the page depending on certain trigger actions instead of using several HTML pages.
+4.
+
+?. ✅ Pop-up for the 'received items'
+
+## Process
+
+I wasn't sure where to start, I had a lot of things in mind, but I decided to break it down into bite-sized pieces. First things first, the sword. I knew dragging to be a chore to get to work properly (as later came to bite me as well), so I thought it might be wise to start with this.
+
+3. Dynamic page loading:
+
+```javascript
+const DYNAMIC_PAGES = {
+  Pages: {
+    "master-sword": `
+      <main class="master-sword-main">
+        <div class="zelda--wrap">
+          <div class="zelda--dialog--wrap">
+            <p class="zelda--name">Rose</p>
+            <div class="zelda--dialog--inner">
+              <p class="zelda--dialog--text">
+                Traveler! Get to know
+                <span class="zelda--highlight">Rose</span> by picking up the
+                <span class="zelda--highlight">Master Sword</span>! Only you can do it..
+              </p>
+            </div>
+            <div class="zelda--triangle"></div>
+          </div>
+        </div>
+
+        <section id="master-sword-container">
+          <section id="master-sword">
+            <img src="./assets/images/sharp-mastersword.png" />
+          </section>
+        </section>
+      </main>
+    `,
+    "item-popup": `<main class="item-popup-main"></main>`,
+    "cards-revealed": `
+      <main class="cards-revealed-main">
+        <h1>View your cards!</h1>
+        <section class="cards-container" role="list" aria-label="Skills List">
+          <section class="flip-card box" tabindex="0" aria-label="Flip Card">
+            <section class="flip-card__front-side" aria-hidden="true">
+              <img class="hyrule-crest" src="./assets/images/hyrule-crest-unedited-removebg-preview.png" alt="Hyrule Crest">
+            </section>
+            <section class="flip-card__back-side" aria-hidden="true"></section>
+          </section>
+          <section class="flip-card box" role="group" tabindex="0" aria-label="Flip Card">
+            <section class="flip-card__front-side" aria-hidden="true">
+              <img class="hyrule-crest" src="./assets/images/hyrule-crest-unedited-removebg-preview.png" alt="Hyrule Crest">
+            </section>
+            <section class="flip-card__back-side" aria-hidden="true"></section>
+          </section>
+          <section class="flip-card box" role="group" tabindex="0" aria-label="Flip Card">
+            <section class="flip-card__front-side" aria-hidden="true">
+              <img class="hyrule-crest" src="./assets/images/hyrule-crest-unedited-removebg-preview.png" alt="Hyrule Crest">
+            </section>
+            <section class="flip-card__back-side" aria-hidden="true"></section>
+          </section>
+        </section>
+      </main>
+    `,
+  },
+};
+```
+
+## External data sources
+
+1. Draggable HTML Element: W3Schools https://www.w3schools.com/howto/howto_js_draggable.asp
 
 <!-- replace the code in the /docs folder with your own, so you can showcase your work with GitHub Pages 🌍 -->
 
--- docs/scripts/script.js
+++ docs/scripts/script.js
@@ -5,46 +5,49 @@
 // VARIABLES & API/JSON FILE CALLS //
 // *********************************
 let dataStorage = {};
-let apiData;
-let imageElement = document.createElement("img");
-let dynamicImagePath;
+// let apiData;
+// let imageElement = document.createElement("img");
+// let dynamicImagePath;
 let flipCard = document.getElementsByClassName("flip-card__back-side");
 let hasInteracted = false;
 let guardianShieldImgPath;
+let p;
+let imgElementUrbosa;
+var guardianData;
 // maak een array aan om later alle opgehaalde data uit de API in te storen
 // dit doe ik zodat ik straks kan loopen adhv de length van deze data.
-let allEntryData = [];
+// let allEntryData = [];
 
 // deze functie neemt als parameter 'entryNumber', dit is het getal van de entry van het karakter dat je wilt oproepen uit de API.
-async function getHyruleCompendiumAPI(entryNumber) {
-  const response = await fetch(
-    `https://botw-compendium.herokuapp.com/api/v3/compendium/entry/${entryNumber}?game=botw`
-  );
-  dataStorage = await response.json();
-  // for ease of access, i store the data json inside another variable called 'data.'
-  // this is to prevent having to call keys out of this variable with 'data.data' later on,
-  // because of how the object is structured.
-  // it's also less confusing for the reader and for myself.
-  apiData = dataStorage.data;
-  dynamicImagePath = apiData.image;
-  // here i log each entry of data i called from the api alongside its respective image in the same log.
-  // console.log(apiData, "IMAGE PATH:", dynamicImagePath);
+// async function getHyruleCompendiumAPI(entryNumber) {
+//   const response = await fetch(
+//     `https://botw-compendium.herokuapp.com/api/v3/compendium/entry/${entryNumber}?game=botw`
+//   );
+//   dataStorage = await response.json();
+//   // for ease of access, i store the data json inside another variable called 'data.'
+//   // this is to prevent having to call keys out of this variable with 'data.data' later on,
+//   // because of how the object is structured.
+//   // it's also less confusing for the reader and for myself.
+//   apiData = dataStorage.data;
+//   dynamicImagePath = apiData.image;
+//   // here i log each entry of data i called from the api alongside its respective image in the same log.
+//   // console.log(apiData, "IMAGE PATH:", dynamicImagePath);
 
-  imageElement.src = `${dynamicImagePath}`;
-  imageElement.alt = `${apiData.name}`;
+//   imageElement.src = `${dynamicImagePath}`;
+//   imageElement.alt = `${apiData.name}`;
 
-  allEntryData.push({
-    entryNumber: entryNumber,
-    apiData: apiData,
-    dynamicImagePath: dynamicImagePath,
-  });
-}
+//   // allEntryData.push({
+//   //   entryNumber: entryNumber,
+//   //   apiData: apiData,
+//   //   dynamicImagePath: dynamicImagePath,
+//   // });
+// }
 
 async function getGuardianInformation() {
   const response = await fetch(
     `https://botw-compendium.herokuapp.com/api/v3/compendium/entry/guardian_stalker`
   );
-  const guardianData = await response.json();
+  guardianData = await response.json();
   console.log("GUARDIAN DATA:", guardianData);
 }
 
@@ -57,36 +60,25 @@ async function getGuardianShield() {
   guardianShieldImgPath = guardianShieldData.data.image;
 }
 
-// async function fetchDataInOrder(entryNumbers) {
-//   for (const entryNumber of entryNumbers) {
-//     await getHyruleCompendiumAPI(entryNumber);
-//   }
-// }
-
 document.addEventListener("DOMContentLoaded", async () => {
-  // Fetch data for the first card
-  await getHyruleCompendiumAPI(155);
-
   await Promise.all([
-    // getHyruleCompendiumAPI(156),
-    // getHyruleCompendiumAPI(157),
-    // getHyruleCompendiumAPI(158),
+    // getHyruleCompendiumAPI(155),
+    getGuardianShield(),
+    getGuardianInformation(),
   ]);
 
   await getPersonalData();
-  await getGuardianShield();
-  await getGuardianInformation();
 });
 
 async function getPersonalData() {
   const response = await fetch("./assets/data-json/personal-data.json");
   personalData = await response.json();
 
-  console.log(
-    "PERSONAL DATA:",
-    personalData.images.find((img) => img.hasOwnProperty("urbosa")).urbosa
-  );
-  console.log("MY BIO", personalData.bio);
+  console.log("MY BIO", personalData.favcharactersbio);
+
+  p = document.createElement("p");
+  p.innerHTML = `${personalData.favcharactersbio}`;
+  console.log("paragraph", p);
 }
 
 // this variable contains all of my dynamic page data for this project, save from of course the API.
@@ -96,59 +88,52 @@ const DYNAMIC_PAGES = {
   Pages: {
     "master-sword": `
       <main class="master-sword-main">
-      <section id="master-sword-container">
-        <section id="master-sword">
-          <img src="./assets/images/sharp-mastersword.png" />
+        <div class="zelda--wrap">
+          <div class="zelda--dialog--wrap">
+            <p class="zelda--name">Rose</p>
+            <div class="zelda--dialog--inner">
+              <p class="zelda--dialog--text">
+                Traveler! Get to know
+                <span class="zelda--highlight">Rose</span> by picking up the
+                <span class="zelda--highlight">Master Sword</span>! Only you can do it..
+              </p>
+            </div>
+            <div class="zelda--triangle"></div>
+          </div>
+        </div>
+
+        <section id="master-sword-container">
+          <section id="master-sword">
+            <img src="./assets/images/sharp-mastersword.png" />
+          </section>
         </section>
-      </section>
-    </main>
+      </main>
     `,
-    "item-popup": ` <main class="item-popup-main"></main>`,
+    "item-popup": `<main class="item-popup-main"></main>`,
     "cards-revealed": `
-      <main>
-  <section class="cards-container" role="list" aria-label="Skills List">
-    <section
-      class="flip-card box"
-      role="button"
-      tabindex="0"
-      aria-label="Flip Card"
-    >
-      <section class="flip-card__front-side" aria-hidden="true">
-      </section>
-      <section class="flip-card__back-side" aria-hidden="true">
-      </section>
-    </section>
-    <section
-      class="flip-card box"
-      role="button"
-      tabindex="0"
-      aria-label="Flip Card"
-    >
-      <section class="flip-card__front-side" aria-hidden="true"></section>
-      <section class="flip-card__back-side" aria-hidden="true"></section>
-    </section>
-    <section
-      class="flip-card box"
-      role="button"
-      tabindex="0"
-      aria-label="Flip Card"
-    >
-      <section class="flip-card__front-side" aria-hidden="true"></section>
-      <section class="flip-card__back-side" aria-hidden="true"></section>
-    </section>
-    <section
-      class="flip-card box"
-      role="button"
-      tabindex="0"
-      aria-label="Flip Card"
-    >
-      <section class="flip-card__front-side" aria-hidden="true">
-      </section>
-      <section class="flip-card__back-side" aria-hidden="true">
-      </section>
-    </section>
-  </section>
-</main>
+      <main class="cards-revealed-main">
+        <h1>View your cards!</h1>
+        <section class="cards-container" role="list" aria-label="Skills List">
+          <section class="flip-card box" tabindex="0" aria-label="Flip Card">
+            <section class="flip-card__front-side" aria-hidden="true">
+              <img class="hyrule-crest" src="./assets/images/hyrule-crest-unedited-removebg-preview.png" alt="Hyrule Crest">
+            </section>
+            <section class="flip-card__back-side" aria-hidden="true"></section>
+          </section>
+          <section class="flip-card box" role="group" tabindex="0" aria-label="Flip Card">
+            <section class="flip-card__front-side" aria-hidden="true">
+              <img class="hyrule-crest" src="./assets/images/hyrule-crest-unedited-removebg-preview.png" alt="Hyrule Crest">
+            </section>
+            <section class="flip-card__back-side" aria-hidden="true"></section>
+          </section>
+          <section class="flip-card box" role="group" tabindex="0" aria-label="Flip Card">
+            <section class="flip-card__front-side" aria-hidden="true">
+              <img class="hyrule-crest" src="./assets/images/hyrule-crest-unedited-removebg-preview.png" alt="Hyrule Crest">
+            </section>
+            <section class="flip-card__back-side" aria-hidden="true"></section>
+          </section>
+        </section>
+      </main>
     `,
   },
 };
@@ -217,7 +202,7 @@ function draggableElement(draggableElementEvent) {
       bodyElement.replaceChild(itemPopup, masterSwordScene);
       bodyElement.classList.remove("master-sword-main-transition");
       function play() {
-        let audio = new Audio("../docs/assets/audio/reveal-audio.mp3");
+        let audio = new Audio("./assets/audio/reveal-audio.mp3");
         audio.play();
       }
       play();
@@ -226,7 +211,7 @@ function draggableElement(draggableElementEvent) {
     function checkHasInteracted() {
       if (hasInteracted === true) {
         function play() {
-          let audio = new Audio("../docs/assets/audio/buildup-audio.mp3");
+          let audio = new Audio("./assets/audio/buildup-audio.mp3");
           audio.play();
         }
         console.log("hasInteracted is true");
@@ -236,10 +221,10 @@ function draggableElement(draggableElementEvent) {
       }
     }
 
-    if (clientY < 500 && !hasInteracted) {
+    if (clientY < 400 && !hasInteracted) {
       hasInteracted = true;
       bodyElement.classList.add("master-sword-main-transition");
-      setTimeout(switchScenes, 1000);
+      setTimeout(switchScenes, 8000);
       checkHasInteracted();
     }
   }
@@ -253,28 +238,105 @@ function draggableElement(draggableElementEvent) {
 }
 
 itemPopup.addEventListener("click", () => {
+  // replaces the html for the item popup scene for the cards scene
   bodyElement.replaceChild(cardsRevealedScene, itemPopup);
-  const secondH2Element = document.createElement("h2");
-  secondH2Element.textContent = `Rose Mulazada`;
-  flipCard[0].appendChild(secondH2Element);
+  flipCard[1].appendChild(p);
 
+  // will later be used to make sure some specific data is properly capitalised, as it isn't done so directly from the api.
+  function capitalizeEachWord(string) {
+    return string.replace(/\b\w/g, (char) => char.toUpperCase());
+  }
+
+  // **************************
+  // flipCard 0: About me! //
+  // *************************
+  const developerName = document.createElement("h2");
+  developerName.textContent = `${personalData.name}`;
+  flipCard[0].appendChild(developerName);
+
+  const personalDataIntroduction =
+    personalData.pagetitles[0].cardsrevealed[0].flipCardOne[0].introduction;
+
+  // Loop over the 'storage' array
+  for (let i = 0; i < personalDataIntroduction.length; i++) {
+    // Create a wrapping div for each set of elements
+    const wrapperDiv = document.createElement("div");
+
+    const developerLabel = document.createElement("h3");
+    developerLabel.textContent = personalDataIntroduction[i].label || ""; // Use 'storage' instead of 'personalData.introduction'
+
+    const developerValue = document.createElement("p");
+    developerValue.textContent = personalDataIntroduction[i].value;
+
+    wrapperDiv.appendChild(developerLabel);
+    wrapperDiv.appendChild(developerValue);
+
+    flipCard[0].appendChild(wrapperDiv);
+  }
+
+  // ********************************************
+  // flipCard 1: Favorite Characters from TLOZ //
+  // *******************************************
+  // CHARACTER NAMES
+  const urbosaCharacter = personalData.characternames.find((character) => {
+    return character.hasOwnProperty("urbosa");
+  }).urbosa;
+
+  const miphaCharacter = personalData.characternames.find((character) => {
+    return character.hasOwnProperty("mipha");
+  }).mipha;
+
+  const guardianStalkerCharacter = capitalizeEachWord(guardianData.data.name);
+
+  const urbosaSpan = document.getElementById("urbosa-span-element");
+  const miphaSpan = document.getElementById("mipha-span-element");
+  const guardianStalkersSpan = document.getElementById(
+    "guardianstalker-span-element"
+  );
+
+  urbosaSpan.innerHTML = urbosaCharacter;
+  miphaSpan.innerHTML = miphaCharacter;
+  guardianStalkersSpan.innerHTML = `${guardianStalkerCharacter}s, (said no one ever..)`;
+
+  //images
+  // retrieve path.
+  const urbosaImgPath = personalData.images.find((img) =>
+    img.hasOwnProperty("urbosa")
+  ).urbosa;
+  console.log("URBOSA IMG PATH", urbosaImgPath);
+  const miphaImgPath = personalData.images.find((img) =>
+    img.hasOwnProperty("mipha")
+  ).mipha;
+  const guardianStalkerImgPath = guardianData.data.image;
+
+  // retrieve alt text.
+  const urbosaAltText = personalData.alttext.find((alt) =>
+    alt.hasOwnProperty("urbosa")
+  ).urbosa;
+  const miphaAltText = personalData.alttext.find((alt) =>
+    alt.hasOwnProperty("mipha")
+  ).mipha;
+  const guardianStalkerAltText = `A Guardian Stalker from Breath of The Wild.`;
+
+  imgElementUrbosa = document.getElementById("urbosa-img-element");
+  imgElementUrbosa.src = `${urbosaImgPath}`;
+  imgElementUrbosa.alt = `${urbosaAltText}`;
+
+  imgElementMipha = document.getElementById("mipha-img-element");
+  imgElementMipha.src = `${miphaImgPath}`;
+  imgElementMipha.alt = `${miphaAltText}`;
+
+  // declare img element.
+  imgElementGuardianStalker = document.getElementById(
+    "guardianstalker-img-element"
+  );
+  imgElementGuardianStalker.src = `${guardianStalkerImgPath}`;
+  imgElementGuardianStalker.alt = `${guardianStalkerAltText}`;
+
+  // flipCard 2: Add other content here if needed
+
+  // flipCard 3: Favorite Other TLOZ Things
   const guardianShieldImg = document.createElement("img");
   guardianShieldImg.src = `${guardianShieldImgPath}`;
-  flipCard[1].appendChild(guardianShieldImg);
-  const dynamicImageEntry = document.createElement("img");
-  const h2Element = document.createElement("h2");
-
-  h2Element.textContent = `${apiData.name}`;
-
-  dynamicImageEntry.src = `${dynamicImagePath}`;
-
-  flipCard[0].appendChild(h2Element);
-  flipCard[0].appendChild(dynamicImageEntry.cloneNode());
-  for (let i = 0; i < flipCard.length; i++) {}
+  flipCard[2].appendChild(guardianShieldImg);
 });
-// audio.play().catch((e) => {
-//   if (e.code === DOMException.ABORT_ERR) {
-//     return
-//   }
-//   throw e;
-// })
